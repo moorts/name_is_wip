bac4c2d9d5e91040c3d4b459fdf3fb4bfb3ee1af
moorts <moritz.gutfleisch@gmx.de>
2021-10-18 17:40:40 +0200

    Remove redundant String::from() calls

--- emulator/src/terminator/disassembler.rs
+++ emulator/src/terminator/disassembler.rs
@@ -69,18 +69,18 @@ impl Disassembler {
         // Jump instructions
         match instr {
             0x00 => Ok(String::from("NOP")),
-            0x01 => Ok(String::from(format!(
+            0x01 => Ok(format!(
                 "LXI B,{}",
                 Disassembler::fmt_hex::<u16>(self.read_addr())
-            ))),
+            )),
             0x02 => Ok(String::from("STAX B")),
             0x03 => Ok(String::from("INX B")),
             0x04 => Ok(String::from("INR B")),
             0x05 => Ok(String::from("DCR B")),
-            0x06 => Ok(String::from(format!(
+            0x06 => Ok(format!(
                 "MVI B,{}",
                 Disassembler::fmt_hex::<u8>(self.read_byte())
-            ))),
+            )),
             0x07 => Ok(String::from("RLC")),
             0x08 => {
                 // No instruction
@@ -91,27 +91,27 @@ impl Disassembler {
             0x0b => Ok(String::from("DCX B")),
             0x0c => Ok(String::from("INR C")),
             0x0d => Ok(String::from("DCR C")),
-            0x0e => Ok(String::from(format!(
+            0x0e => Ok(format!(
                 "MVI C,{}",
                 Disassembler::fmt_hex::<u8>(self.read_byte())
-            ))),
+            )),
             0x0f => Ok(String::from("RRC")),
             0x10 => {
                 // No instruction
                 Err("Invalid opcode")
             }
-            0x11 => Ok(String::from(format!(
+            0x11 => Ok(format!(
                 "LXI D,{}",
                 Disassembler::fmt_hex::<u16>(self.read_addr())
-            ))),
+            )),
             0x12 => Ok(String::from("STAX D")),
             0x13 => Ok(String::from("INX D")),
             0x14 => Ok(String::from("INR D")),
             0x15 => Ok(String::from("DCR D")),
-            0x16 => Ok(String::from(format!(
+            0x16 => Ok(format!(
                 "MVI D,{}",
                 Disassembler::fmt_hex::<u8>(self.read_byte())
-            ))),
+            )),
             0x17 => Ok(String::from("RAL")),
             0x18 => {
                 // No instruction
@@ -122,78 +122,78 @@ impl Disassembler {
             0x1b => Ok(String::from("DCX D")),
             0x1c => Ok(String::from("INR E")),
             0x1d => Ok(String::from("DCR E")),
-            0x1e => Ok(String::from(format!(
+            0x1e => Ok(format!(
                 "MVI E,{}",
                 Disassembler::fmt_hex::<u8>(self.read_byte())
-            ))),
+            )),
             0x1f => Ok(String::from("RAR")),
             0x20 => Ok(String::from("RIM")),
-            0x21 => Ok(String::from(format!(
+            0x21 => Ok(format!(
                 "LXI H,{}",
                 Disassembler::fmt_hex::<u16>(self.read_addr())
-            ))),
-            0x22 => Ok(String::from(format!(
+            )),
+            0x22 => Ok(format!(
                 "SHLD {}",
                 Disassembler::fmt_hex::<u16>(self.read_addr())
-            ))),
+            )),
             0x23 => Ok(String::from("INX H")),
             0x24 => Ok(String::from("INR H")),
             0x25 => Ok(String::from("DCR H")),
-            0x26 => Ok(String::from(format!(
+            0x26 => Ok(format!(
                 "MVI H,{}",
                 Disassembler::fmt_hex::<u8>(self.read_byte())
-            ))),
+            )),
             0x27 => Ok(String::from("DAA")),
             0x28 => {
                 // No instruction
                 Err("Invalid opcode")
             }
             0x29 => Ok(String::from("DAD H")),
-            0x2a => Ok(String::from(format!(
+            0x2a => Ok(format!(
                 "LHLD {}",
                 Disassembler::fmt_hex::<u16>(self.read_addr())
-            ))),
+            )),
             0x2b => Ok(String::from("DCX H")),
             0x2c => Ok(String::from("INR L")),
             0x2d => Ok(String::from("DCR L")),
-            0x2e => Ok(String::from(format!(
+            0x2e => Ok(format!(
                 "MVI L,{}",
                 Disassembler::fmt_hex::<u8>(self.read_byte())
-            ))),
+            )),
             0x2f => Ok(String::from("CMA")),
             0x30 => Ok(String::from("SIM")),
-            0x31 => Ok(String::from(format!(
+            0x31 => Ok(format!(
                 "LXI SP,{}",
                 Disassembler::fmt_hex::<u16>(self.read_addr())
-            ))),
-            0x32 => Ok(String::from(format!(
+            )),
+            0x32 => Ok(format!(
                 "STA {}",
                 Disassembler::fmt_hex::<u16>(self.read_addr())
-            ))),
+            )),
             0x33 => Ok(String::from("INX SP")),
             0x34 => Ok(String::from("INR M")),
             0x35 => Ok(String::from("DCR M")),
-            0x36 => Ok(String::from(format!(
+            0x36 => Ok(format!(
                 "MVI M,{}",
                 Disassembler::fmt_hex::<u8>(self.read_byte())
-            ))),
+            )),
             0x37 => Ok(String::from("STC")),
             0x38 => {
                 // No instruction
                 Err("Invalid opcode")
             }
             0x39 => Ok(String::from("DAD SP")),
-            0x3a => Ok(String::from(format!(
+            0x3a => Ok(format!(
                 "LDA {}",
                 Disassembler::fmt_hex::<u16>(self.read_addr())
-            ))),
+            )),
             0x3b => Ok(String::from("DCX SP")),
             0x3c => Ok(String::from("INR A")),
             0x3d => Ok(String::from("DCR A")),
-            0x3e => Ok(String::from(format!(
+            0x3e => Ok(format!(
                 "MVI A,{}",
                 Disassembler::fmt_hex::<u8>(self.read_byte())
-            ))),
+            )),
             0x3f => Ok(String::from("CMC")),
             0x40 => Ok(String::from("MOV B,B")),
             0x41 => Ok(String::from("MOV B,C")),
@@ -325,166 +325,166 @@ impl Disassembler {
             0xbf => Ok(String::from("CMP A")),
             0xc0 => Ok(String::from("RNZ")),
             0xc1 => Ok(String::from("POP B")),
-            0xc2 => Ok(String::from(format!(
+            0xc2 => Ok(format!(
                 "JNZ {}",
                 Disassembler::fmt_hex::<u16>(self.read_addr())
-            ))),
-            0xc3 => Ok(String::from(format!(
+            )),
+            0xc3 => Ok(format!(
                 "JMP {}",
                 Disassembler::fmt_hex::<u16>(self.read_addr())
-            ))),
-            0xc4 => Ok(String::from(format!(
+            )),
+            0xc4 => Ok(format!(
                 "CNZ {}",
                 Disassembler::fmt_hex::<u16>(self.read_addr())
-            ))),
+            )),
             0xc5 => Ok(String::from("PUSH B")),
-            0xc6 => Ok(String::from(format!(
+            0xc6 => Ok(format!(
                 "ADI {}",
                 Disassembler::fmt_hex::<u8>(self.read_byte())
-            ))),
+            )),
             0xc7 => Ok(String::from("RST 0")),
             0xc8 => Ok(String::from("RZ")),
             0xc9 => Ok(String::from("RET")),
-            0xca => Ok(String::from(format!(
+            0xca => Ok(format!(
                 "JZ {}",
                 Disassembler::fmt_hex::<u16>(self.read_addr())
-            ))),
+            )),
             0xcb => {
                 // No instruction
                 Err("Invalid opcode")
             }
-            0xcc => Ok(String::from(format!(
+            0xcc => Ok(format!(
                 "CZ {}",
                 Disassembler::fmt_hex::<u16>(self.read_addr())
-            ))),
-            0xcd => Ok(String::from(format!(
+            )),
+            0xcd => Ok(format!(
                 "CALL {}",
                 Disassembler::fmt_hex::<u16>(self.read_addr())
-            ))),
-            0xce => Ok(String::from(format!(
+            )),
+            0xce => Ok(format!(
                 "ACI {}",
                 Disassembler::fmt_hex::<u8>(self.read_byte())
-            ))),
+            )),
             0xcf => Ok(String::from("RST 1")),
             0xd0 => Ok(String::from("RNC")),
             0xd1 => Ok(String::from("POP D")),
-            0xd2 => Ok(String::from(format!(
+            0xd2 => Ok(format!(
                 "JNC {}",
                 Disassembler::fmt_hex::<u16>(self.read_addr())
-            ))),
-            0xd3 => Ok(String::from(format!(
+            )),
+            0xd3 => Ok(format!(
                 "OUT {}",
                 Disassembler::fmt_hex::<u8>(self.read_byte())
-            ))),
-            0xd4 => Ok(String::from(format!(
+            )),
+            0xd4 => Ok(format!(
                 "CNC {}",
                 Disassembler::fmt_hex::<u16>(self.read_addr())
-            ))),
+            )),
             0xd5 => Ok(String::from("PUSH D")),
-            0xd6 => Ok(String::from(format!(
+            0xd6 => Ok(format!(
                 "SUI {}",
                 Disassembler::fmt_hex::<u8>(self.read_byte())
-            ))),
+            )),
             0xd7 => Ok(String::from("RST 2")),
             0xd8 => Ok(String::from("RC")),
             0xd9 => {
                 // No instruction
                 Err("Invalid opcode")
             }
-            0xda => Ok(String::from(format!(
+            0xda => Ok(format!(
                 "JC {}",
                 Disassembler::fmt_hex::<u16>(self.read_addr())
-            ))),
-            0xdb => Ok(String::from(format!(
+            )),
+            0xdb => Ok(format!(
                 "IN {}",
                 Disassembler::fmt_hex::<u8>(self.read_byte())
-            ))),
-            0xdc => Ok(String::from(format!(
+            )),
+            0xdc => Ok(format!(
                 "CC {}",
                 Disassembler::fmt_hex::<u16>(self.read_addr())
-            ))),
+            )),
             0xdd => {
                 // No instruction
                 Err("Invalid opcode")
             }
-            0xde => Ok(String::from(format!(
+            0xde => Ok(format!(
                 "SBI {}",
                 Disassembler::fmt_hex::<u8>(self.read_byte())
-            ))),
+            )),
             0xdf => Ok(String::from("RST 3")),
             0xe0 => Ok(String::from("RPO")),
             0xe1 => Ok(String::from("POP H")),
-            0xe2 => Ok(String::from(format!(
+            0xe2 => Ok(format!(
                 "JPO {}",
                 Disassembler::fmt_hex::<u16>(self.read_addr())
-            ))),
+            )),
             0xe3 => Ok(String::from("XTHL")),
-            0xe4 => Ok(String::from(format!(
+            0xe4 => Ok(format!(
                 "CPO {}",
                 Disassembler::fmt_hex::<u16>(self.read_addr())
-            ))),
+            )),
             0xe5 => Ok(String::from("PUSH H")),
-            0xe6 => Ok(String::from(format!(
+            0xe6 => Ok(format!(
                 "ANI {}",
                 Disassembler::fmt_hex::<u8>(self.read_byte())
-            ))),
+            )),
             0xe7 => Ok(String::from("RST 4")),
             0xe8 => Ok(String::from("RPE")),
             0xe9 => Ok(String::from("PCHL")),
-            0xea => Ok(String::from(format!(
+            0xea => Ok(format!(
                 "JPE {}",
                 Disassembler::fmt_hex::<u16>(self.read_addr())
-            ))),
+            )),
             0xeb => Ok(String::from("XCHG")),
-            0xec => Ok(String::from(format!(
+            0xec => Ok(format!(
                 "CPE {}",
                 Disassembler::fmt_hex::<u16>(self.read_addr())
-            ))),
+            )),
             0xed => {
                 // No instruction
                 Err("Invalid opcode")
             }
-            0xee => Ok(String::from(format!(
+            0xee => Ok(format!(
                 "XRI {}",
                 Disassembler::fmt_hex::<u8>(self.read_byte())
-            ))),
+            )),
             0xef => Ok(String::from("RST 5")),
             0xf0 => Ok(String::from("RP")),
             0xf1 => Ok(String::from("POP PSW")),
-            0xf2 => Ok(String::from(format!(
+            0xf2 => Ok(format!(
                 "JP {}",
                 Disassembler::fmt_hex::<u16>(self.read_addr())
-            ))),
+            )),
             0xf3 => Ok(String::from("DI")),
-            0xf4 => Ok(String::from(format!(
+            0xf4 => Ok(format!(
                 "CP {}",
                 Disassembler::fmt_hex::<u16>(self.read_addr())
-            ))),
+            )),
             0xf5 => Ok(String::from("PUSH PSW")),
-            0xf6 => Ok(String::from(format!(
+            0xf6 => Ok(format!(
                 "ORI {}",
                 Disassembler::fmt_hex::<u8>(self.read_byte())
-            ))),
+            )),
             0xf7 => Ok(String::from("RST 6")),
             0xf8 => Ok(String::from("RM")),
             0xf9 => Ok(String::from("SPHL")),
-            0xfa => Ok(String::from(format!(
+            0xfa => Ok(format!(
                 "JM {}",
                 Disassembler::fmt_hex::<u16>(self.read_addr())
-            ))),
+            )),
             0xfb => Ok(String::from("EI")),
-            0xfc => Ok(String::from(format!(
+            0xfc => Ok(format!(
                 "CM {}",
                 Disassembler::fmt_hex::<u16>(self.read_addr())
-            ))),
+            )),
             0xfd => {
                 // No instruction
                 Err("Invalid opcode")
             }
-            0xfe => Ok(String::from(format!(
+            0xfe => Ok(format!(
                 "CPI {}",
                 Disassembler::fmt_hex::<u8>(self.read_byte())
-            ))),
+            )),
             0xff => Ok(String::from("RST 7")),
         }
     }
