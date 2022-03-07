a6bcb5498cf23ca4b64b21c17f342bb6d49cad17
Nico Thomas <nico.thomas@sap.com>
2022-03-07 15:52:55 +0100

    Add emulator wasm file to angular project

--- emulator-webapp/src/app/app.component.ts
+++ emulator-webapp/src/app/app.component.ts
@@ -1,12 +1,20 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
+import init from "emulator";
 
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
   styleUrls: ['./app.component.less']
 })
-export class AppComponent {
+export class AppComponent implements OnInit {
+
   title = 'emulator-webapp';
   editorOptions = {theme: 'vs-dark', language: 'javascript'};
   code: string = 'function x() {\nconsole.log("Hello world!");\n}';
+
+  async ngOnInit(): Promise<void> {
+    console.log("AppComponent OnInit!");
+    let a = await init("assets/emulator/emulator_bg.wasm");
+    a.greet();
+  }
 }
